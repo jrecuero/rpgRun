d26e424ccf363a9fbf824039b6efc33dd6b80373
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-10-15 22:50:42 -0700

    Reorder directories

--- tplay.sh
+++ tplay.sh
@@ -2,9 +2,9 @@
 
 source ./setup.sh
 
-export PYTHONPATH=${RPG_RUN_PATH}:${TEXT_PLAY_PATH}:${JC2LI_PATH}
+export PYTHONPATH=${CURRENT_DIRECTORY}:${RPG_RUN_PATH}:${TEXT_PLAY_PATH}:${JC2LI_PATH}
 
 TEXT_PLAY_APP=play.py
 
 #python ${TEXT_PLAY_PATH}/${TEXT_PLAY_APP}
-python ${JC2LI_PATH}/run.py -M textplay.play
+python ${JC2LI_PATH}/run.py -M play.text.play
