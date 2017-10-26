79157e7780f72b2c8301a481e17f4ad642745627
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-10-26 13:28:04 -0700

    Update vi script with path

--- vi.sh
+++ vi.sh
@@ -2,8 +2,9 @@
 
 CURRENT_DIR=`pwd`
 REPO_PATH=${HOME}/Repository
+APP_PATH=REPO_PATH/rpgRun
 PYGAME_PATH=/Users/jorecuer/virtualenv/python3/lib/python3.6/site-packages
 
 export PYTHONPATH=$PYTHONPATH:$PYGAME_PATH
 
-vi $1
+vim $1
