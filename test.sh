6729b58823e5186808d70f1f649f92eb47072a0f
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-11-13 09:01:51 -0800

    Update doctest setup

--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 
 source ./setup.sh
 
-py.test test/ --doctest-modules --ignore=textplay --cov-report html --cov=./rpgrun
+export PYTHONPATH=${CURRENT_DIRECTORY}:${JC2LI_PATH}
+
+# py.test test/ --doctest-modules --ignore=textplay --cov-report html --cov=./rpgrun
+py.test ./rpgrun/ --doctest-modules --ignore=textplay --cov-report html --cov=./rpgrun
